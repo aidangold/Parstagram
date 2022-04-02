c06dd8e5dc6c075b429174c7987918b94aad25a3
aidangold <aidanpaulgoldstein@gmail.com>
2022-04-02 02:51:57 -0400

    part two functions

---
+++ app/src/main/java/com/example/parstagram/fragments/FeedFragment.kt
@@ -0,0 +1,76 @@
+package com.example.parstagram.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parstagram.MainActivity
+import com.example.parstagram.Post
+import com.example.parstagram.PostAdapter
+import com.example.parstagram.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+
+open class FeedFragment : Fragment() {
+
+    lateinit var postsRecyclerView: RecyclerView
+
+    lateinit var adapter: PostAdapter
+
+    var allPosts: MutableList<Post> = mutableListOf()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // this is where we will set up our views and click listeners
+
+        postsRecyclerView = view.findViewById(R.id.postRecyclerView)
+        // sets adapter to recyclerview
+        adapter = PostAdapter(requireContext(), allPosts)
+        postsRecyclerView.adapter = adapter
+
+        postsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        queryPosts()
+    }
+
+    // Query for all posts in our server
+    open fun queryPosts() {
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.findInBackground(object : FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null) {
+                    // uh oh something wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null) {
+                        for (post in posts) {
+                            Log.i(TAG, "Post: " + post.getDescription() + " , username: " +
+                                    post.getUser()?.username
+                            )
+                        }
+
+                        allPosts.addAll(posts)
+                        //adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        })
+    }
+    companion object {
+        const val TAG = "FeedFragment"
+    }
+}
